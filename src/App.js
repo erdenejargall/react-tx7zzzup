21d1e7b269fd20e6751656406bd90e86bd11029b
erdenejargall <batragchaaerdenejargal@gmail.com>
2025-11-09 18:53:24 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { useState } from 'react';
 
 export default function Calc() {
@@ -7,7 +5,7 @@ export default function Calc() {
   const [sum, setSum] = useState(0);
   const [max, setMax] = useState(0);
   const [average, setAverage] = useState(0);
-  const [input, setInput] = useState('');
+  const [num, setNum] = useState('');
 
   const sumr = (a, b) => setSum(a + b);
   const maxr = (a, b, c) => setMax(Math.max(a, b, c));
@@ -15,9 +13,9 @@ export default function Calc() {
 
   const datar = () => {
     const newData = [...data];
-    newData.push(Number(input));
+    newData.push(Number(num));
     setData(newData);
-    setInput('');
+    setNum('');
   };
 
   const Submit = () => {
@@ -32,13 +30,13 @@ export default function Calc() {
     <div>
       <input
         type="number"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        value={num}
+        onChange={(e) => setNum(e.target.value)}
         placeholder="Enter number"
       />
-      <button onClick={datar}>Add</button>
+      <button onClick={datar} className='w-24 h-12 border-2 mx-4'>Add</button>
 
-      <button onClick={Submit}>Submit</button>
+      <button onClick={Submit} className='w-24 h-12 border-2'>Submit</button>
 
       <p>
         Sum: <span>{sum}</span>
